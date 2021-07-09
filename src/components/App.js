353a6616f13d05241ef6b4b5475dbcda18d98061
Volodymyr Moruhin <vova.morugin@gmail.com>
2021-07-09 16:57:36 -0700

    add component to render teams and players

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,27 @@
 import * as React from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom'
 
-export default function App () {
+import Home from './Home'
+import Players from './Players'
+import Teams from './Teams'
+import Navbar from './Navbar'
+
+export default function App() {
   return (
-    <div>
-      Hash History Basketball League
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Switch>
+          <Route exact path='/'> <Home /></Route>
+          <Route path='/players'><Players /></Route>
+          <Route path='/teams'><Teams /></Route>
+          <Route path='*'><h1 className='text-center'>Four oh Four</h1></Route>
+        </Switch>
+      </div>
+    </Router>
   )
 }
